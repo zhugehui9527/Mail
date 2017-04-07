b5ce954b9775d7b9564c98d1e457dfe5e1e6f5c4
zhugehui9527 <zhugehui9527@users.noreply.github.com>
2017-04-07 15:24:35 +0800

    Update Run.py
    注销账号

--- Run.py
+++ Run.py
@@ -2,21 +2,15 @@
 from run.Mail import *
 
 # 第三方 SMTP 服务
-# mail_host = "smtp.163.com"  # 设置服务器
-# mail_user="18589091413@163.com"    #用户名
-# mail_pass="zhang6246593"   #口令
-
-# mail_user="2571226011@qq.com"    #用户名
-# mail_pass="qmyjbocqbmoyecbe"   #授权码
 
 mail_host = "smtp.qq.com"  # 设置服务器
-mail_user = "372233740@qq.com"  # 用户名
-mail_pass = "hcpsrobckaetbjbi"  # 授权码
+mail_user = "********@qq.com"  # 用户名
+mail_pass = "********"  # 授权码
 prefix = '@qq.com'  # 指定邮件后缀
 mail_port = 465
 sender = mail_user
-# receivers = ['373743261@qq.com','18589091413@163.com','575735@qq.com','372233740@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-receivers = get_mail_list(373743261, 1, prefix)
+ # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+receivers = get_mail_list(********8, 1, prefix)
 subject = "新 宝 集 团"
 mail_msg = """
 	 <p>
@@ -30,7 +24,7 @@ mail_msg = """
 	 云龙团队，信誉 稳定 安全 <br>
 	 给力计划！软件  应有尽有！<br>
 	<br>
-	做新宝认准 云龙 qq：575735 <br>
+	做新宝认准 云龙 qq：****** <br>
 	</p>
 	 <p><a href="http://666.xbgg1.com/index.php?s=/WebPublic/register/code/31849b9a2d34228cf4cb024bf9fa07ff/plf/link/">快速入口</a></p>
 
